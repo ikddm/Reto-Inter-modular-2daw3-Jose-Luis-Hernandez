90545e2cf2c67f7cb756bad42b738f79b615a5c2
Jose <ikddm@plaiaundi.com>
2024-01-11 10:00:40 +0100

    cambios 11/1

---
+++ script.js
@@ -0,0 +1,52 @@
+  // Inicializar el mapa y establecer las coordenadas y el nivel de zoom
+  var map = L.map('mapid').setView([43.139, -2.20], 9);
+
+
+  // Añadir una capa de tiles de OpenStreetMap
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+  }).addTo(map);
+
+    var lugares = [
+        { "nombre": "Donosti", "latitud": 43.3183, "longitud": -1.9812 },
+        { "nombre": "Bilbao", "latitud": 43.2630, "longitud": -2.9350 },
+        { "nombre": "Vitoria", "latitud": 42.8595, "longitud": -2.6818 }
+    ]
+
+lugares.forEach(lugar => {
+    var marker = L.marker([lugar.latitud, lugar.longitud]).addTo(map);
+    marker.bindTooltip(lugar.nombre, {
+        permanent: false,
+        direction: 'top',
+        offset: L.point(0, -20)
+    });
+    marker.on('click', function() {
+    console.log('Click' +lugar.nombre);
+});
+});
+
+const xValues = [100,200,300,400,500,600,700];
+
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{ 
+        data: [100,200],
+      borderColor: "red",
+      fill: false
+    }, { 
+        data: [100,200],
+      borderColor: "green",
+      fill: false
+    }, { 
+      data: [100,200],
+      borderColor: "blue",
+      fill: false
+    }]
+  },
+  options: {
+    legend: {display: false}
+  }
+});
+
